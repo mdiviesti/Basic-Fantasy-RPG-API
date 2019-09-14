8ca639091f6cb2a1b313c010af3e85ac15e0b2ef
mdiviesti <mediviesti@gmail.com>
2019-09-13 22:19:45 -0500

    Added the core spells. I borrowed the JSON from https://github.com/humburg/bfrpg_spell_cards but they do need some editing as there's html in the response and I want this to be raw data

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 let racesData = require('./modules/RacesData');
-let classesDate = require('./modules/ClassesData');
+let classesData = require('./modules/ClassesData');
+let spellsData = require('./modules/SpellsData');
 var cors = require('cors');
 
 var RacesData = new racesData();
-var ClassesData = new classesDate();
+var ClassesData = new classesData();
+var SpellsData = new spellsData();
 var express = require('express');
 var restapi = express();
 
@@ -25,6 +27,15 @@ restapi.get('/', function (request, response) {
             "/classes/names": "Get all the class names from the dataset",
             "/classes/names/core": "Get only core class names from the dataset",
             "/classes/name/:name": "Replace :name with a class name to get that specific class record"
+        }
+    }, {
+        "spells": {
+            "/spells": "Get all spells.",
+            "/spells/core": "Get core spells",
+            "/spells/class/:name": "Get spells by class name",
+            "/spells/level/:level": "Get spells by level",
+            "/spells/name/:name": "Get spells by name",
+            "/spells/class/:name/level/:level": "Get spells by class and level"
         }
     }];
     response.json(endpoints);
@@ -77,6 +88,39 @@ restapi.get('/classes/names/core', function (request, response) {
 restapi.get('/classes/name/:name', function (request, response) {
     let name = request.params.name;
     response.json(ClassesData.getClassByName(name));
+});
+
+/**
+ * spells endpoints
+ */
+
+restapi.get('/spells', function (request, response) {
+    response.json(SpellsData.getSpells());
+});
+
+restapi.get('/spells/core', function (request, response) {
+    response.json(SpellsData.getSpellsCore());
+});
+
+restapi.get('/spells/class/:name', function (request, response){
+    let name = request.params.name;
+    response.json(SpellsData.getSpellsByClass(name));
+});
+
+restapi.get('/spells/level/:level', function (request, response){
+    let level = request.params.level;
+    response.json(SpellsData.getSpellsByLevel(level));
+});
+
+restapi.get('/spells/name/:name', function (request, response){
+    let name = request.params.name;
+    response.json(SpellsData.getSpellsByName(name));
+});
+
+restapi.get('/spells/class/:name/level/:level', function (request, response){
+    let name = request.params.name;
+    let level = request.params.level;
+    response.json(SpellsData.getSpellsByClassAndLevel(name, level));
 });
 
 restapi.listen(3000, function () {
